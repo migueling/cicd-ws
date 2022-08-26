b6f683462d8446f79ab9386cdd5ad0748c77d900
Miguel Sanchez <ing.miguels@gmail.com>
2022-08-26 12:54:19 -0500

    Added greetings controller

---
+++ src/main/java/com/itj/cicddemo/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.itj.cicddemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+  @GetMapping("/greetings")
+  public String greetings() {
+    return "Hi everyone!";
+  }
+
+}
